43f939e10ccff60b7b5cdafb78a2c2ac698aa458
Dhrumil Shah <dhrumilshah6211@gmail.com>
2018-04-15 16:08:16 +0530

    insertion sort has been implemeted.

---
+++ sorting/selection_sort.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selection_sort(int *arr, int n) {
+    int i,j;
+    for(i=0;i<n;i++) {
+        int min = arr[i+1];
+        int index;
+        for(j=i+1;j<n;j++) {
+            if(min > arr[j]){
+                min = arr[j];
+                index = j;
+            }
+        }
+        swap(&arr[i], &arr[index]);
+    }
+}
+
+void display(int *arr, int n) {
+    int i;
+    for(i=0;i<n;i++) {
+        printf("%d\t", arr[i]);
+    }
+    printf("\n");
+}
+
+int main() {
+    int n;
+    printf("Please enter the size of the array:");
+    scanf("%d", &n);
+    int arr[n];
+    int i;
+    for(i=0;i<n;i++){
+        printf("Enter element arr[%d]:", i);
+        scanf("%d", arr+i);
+    }
+    selection_sort(arr, n);
+
+    printf("\nSorted Array:\n");
+    display(arr, n);
+    return 0;
+}
